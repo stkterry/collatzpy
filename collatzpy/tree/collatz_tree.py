10d1005a8eeba8d2956961709b86019150887e2c
Steven K. Terry <stkterry@gmail.com>
2020-01-31 21:36:00 -0500

    intial push of experimental branch

--- collatzpy/tree/collatz_tree.py
+++ collatzpy/tree/collatz_tree.py
@@ -2,23 +2,23 @@ from .collatz_node import CollatzNode as CNode
 from itertools import count
 import numpy as np
 
-class CollatzTree:
+class CollatzTree(dict):
 
   def __init__(self):
     self.__root = CNode(1)
     self.__best_node = self.__root
-    self.__refs = {1: self.__root}
+    self[1] = self.__root
 
   def __call__(self, n):
-    if n in self.__refs:
-      return self.__refs[n]
+    if n in self:
+      return self[n]
   
   def has(self, n):
-    return n in self.__refs
+    return n in self
 
   def splay(self, n):
     if not self.has(n): return
-    node = self.__refs[n]
+    node = self[n]
     info = {'n': node.n, 'seq_len': node.seq_len,
             'next': node.next.n}
     return info
@@ -33,7 +33,7 @@ class CollatzTree:
 
   def __add_node(self, n):
     node = CNode(n)
-    self.__refs[n] = node
+    self[n] = node
     return node
 
   def collect(self, n):
@@ -63,6 +63,34 @@ class CollatzTree:
       node = node.next
       seq += 1
 
+  def collect2(self, n):
+    if n in self: return
+
+    sn = n
+    seq = 1
+    node = self.__add_node(n)
+
+    while True:
+      n = self.calc_next(n)
+
+      if n in self:
+        node.next = self[n]
+
+        seq_len = node.next.seq_len
+        node = self[n]
+        while seq > 0:
+          node.seq_len = seq_len + seq
+          node = node.next
+          seq -= 1
+
+        if self[n].seq_len >= self.__best_node.seq_len:
+          self.__best_node = self[sn]
+        break
+
+      node.next = self.__add_node(n)
+      node = node.next
+      seq += 1
+
 
   def collect_from_range(self, a, b):
     a = 2 if a < 2 else a
@@ -72,10 +100,14 @@ class CollatzTree:
     nlist = [n for n in nlist if n > 1]
     for n in nlist: self.collect(n)
 
-  def path(self, n):
-    if not n in self.__refs: return []
+  def collect_from_list2(self, nlist):
+    nlist = [n for n in nlist if n > 1]
+    for n in nlist: self.collect(n)
 
-    node = self.__refs[n]
+  def path(self, n):
+    if not n in self: return []
+
+    node = self[n]
     path = [None] * (node.seq_len + 1)
     for i in count():
       path[i] = node.n
@@ -88,7 +120,7 @@ class CollatzTree:
   def longest_seq(self):
     n = None
     seq_len = 0
-    for i, node in self.__refs.items():
+    for i, node in self.items():
       if node.seq_len > seq_len:
         seq_len = node.seq_len
         n = i
@@ -96,4 +128,4 @@ class CollatzTree:
     return {'n': n, 'seq_len': seq_len}
 
   def size(self):
-    return len(self.__refs)
+    return len(self)
